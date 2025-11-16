d3ad40901dbce129c2e2f8b22b4d6da80126f3d3
Md Rafi Akhtar <alimdrafi@gmail.com>
2025-11-16 19:40:20 +0530

    added code for singleton pattern

---
+++ catalog/01-creational-patterns/05-singleton.ts
@@ -0,0 +1,26 @@
+class Singleton {
+  static instance: Singleton;
+  public id: string;
+
+  private constructor() {
+    this.id = Math.random().toString();
+  }
+
+  static getOrCreateInstance(): Singleton {
+    if (!Singleton.instance) {
+      Singleton.instance = new Singleton();
+    }
+    return Singleton.instance;
+  }
+}
+
+function clientCodeSingleton() {
+  const s1 = Singleton.getOrCreateInstance();
+  const s2 = Singleton.getOrCreateInstance();
+
+  s1.id === s2.id
+    ? console.log(`Both instances have the same id: ${s1.id}`)
+    : console.log(`Both instances have different ids: ${s1.id} and ${s2.id}`);
+}
+clientCodeSingleton();
+clientCodeSingleton();
